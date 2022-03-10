b0c63dad1c5b848cf785e318a44f71092d693e82
wyman1990x <wyman1990x@gmail.com>
2022-03-10 10:46:18 +0800

    sync

--- push.bat
+++ push.bat
@@ -3,7 +3,7 @@ git add -A .
 git commit -m "sync"
 git push origin master
 Xcopy  .\_site ..\blog_release  /E/C/I/Y/S/D  /exclude:excludedfileslist.txt
-Xcopy  _site\index.html ..\blog_release\index.html
+Xcopy  .\_site\index.html ..\blog_release\index.html /Y
 cd ..\blog_release
 git add -A .
 git commit -m "sync"
